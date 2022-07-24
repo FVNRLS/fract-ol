cd9a02d9ee56b9fbfd3a458496065fc2a965927c
Roman Mazuritskiy <rmazurit@2-F-2.42heilbronn.de>
2022-07-24 11:30:37 +0200

    color checker updated

--- tools/ft_strcmp.c
+++ tools/ft_strcmp.c
@@ -20,24 +20,12 @@ This function return values that are as follows −
 
 #include <stddef.h>
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strcmp(char *s1, char *s2)
 {
-	unsigned int	i;
-	unsigned char	*str1;
-	unsigned char	*str2;
+    int i;
 
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
-	i = 0;
-	while (((*str1 != '\0') || (*str2 != '\0')) && (i < n))
-	{
-		if (*str1 < *str2)
-			return (-1);
-		else if (*str1 > *str2)
-			return (1);
-		str1++;
-		str2++;
-		i++;
-	}
-	return (0);
+    i = 0;
+    while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+        i++;
+    return (s1[i] - s2[i]);
 }
